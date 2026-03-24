149ea63e0c33b208385ac758156478ce5843bc89
ibrvtk <nicecock@disroot.org>
2026-03-24 23:08:04 +0200

    Complete ability to edit message's text
    Added API request for editing message's text

--- src/endpoints.py
+++ src/endpoints.py
@@ -12,10 +12,19 @@ def register_endpoints(app: FastAPI):
     def send_messsage(id_sender: int, id_chat: int, text: str) -> tuple[float, str]:
         date, text = db.send_messsage(id_sender, id_chat, text)
 
-        if text is None:
-            return None
+        if (date and text) is None:
+            return {"status": "error", "details": 'Ошибка на уровне БД. Проверьте логи'}
 
         return {'date': date, 'text': text}
+    
+    @app.get('/edit_message/{id_mess}/{new_text}')
+    def edit_message(id_mess: int, new_text: str):
+        date_edit, new_text = db.edit_message(id_mess, new_text)
+
+        if (date_edit and new_text) is None:
+            return {"status": "error", "details": 'Ошибка на уровне БД. Проверьте логи'}
+
+        return {'date_edit': date_edit, 'new_text': new_text}
     
     @app.get('/get_chats/{id_user}')
     def get_chats(id_user: int) -> list:
